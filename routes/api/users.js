c436c28c18c1481e5bbaab335e91f558292b4dd9
marcell <ciszekmarcell@gmail.com>
2019-06-12 20:18:51 +0200

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const gravatar = require('gravatar');
+const bCrypt = require('bcryptjs');
 
 const router = express.Router();
 // validation
@@ -7,6 +9,8 @@ const { check, validationResult } = require('express-validator/check');
 // @Route Post api/users
 // @desc Register User
 // @access public
+
+const User = require('../../models/User');
 
 router.post(
   '/',
@@ -22,8 +26,40 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
+    // req.body - gives us the the data
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'user already exists!' }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      const salt = await bCrypt.genSalt(10);
+      user.password = await bCrypt.hash(password, salt);
+      await user.save();
+
+      res.send('User Registered!');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
     }
   }
 );
