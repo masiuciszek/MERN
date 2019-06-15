c8d298943e865cb58410f9b4d0e44bde0da0e002
marcell <ciszekmarcell@gmail.com>
2019-06-15 13:42:59 +0200

    server fixes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -34,7 +34,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
@@ -63,13 +63,16 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 360000 },
+        { expiresIn: 3600 },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
         }
       );
-    } catch (error) {}
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
